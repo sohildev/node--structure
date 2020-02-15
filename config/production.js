2dbb13e16932c6ae22b98aa7bfba274d0d6e96bc
sohildev <58003771+sohildev@users.noreply.github.com>
2020-02-15 16:15:32 +0530

    Update production.js

--- config/production.js
+++ config/production.js
@@ -7,7 +7,7 @@ module.exports = {
   ip: process.env.IP || `https://127.0.0.1/4000`,
   imagepath: '/public/',
   jwtIssuer: 'www.yourdomainname.com',
-  jwtToken:'hvjfhgfdgdfjkghncm,vngf5656454gfdg4d',
+  jwtToken:'hvjfhgfdgdfjkghncmvngf5656454gfdg4d',
   jwtExpires: 86400 // Numeric seconds expiry (86400 = '1d'). If string supplied, you provide units (e.g., "8h")
 
 };
