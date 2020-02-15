619dc00a48c45d8075838ec29e48168e2f26db27
sohildev <58003771+sohildev@users.noreply.github.com>
2020-02-15 16:16:33 +0530

    Update development.js

--- config/development.js
+++ config/development.js
@@ -2,13 +2,14 @@
 
 module.exports = {
   env: 'development',
-  db: 'mongodb+srv://sohil:sohiL@5240@cluster0-kjwrv.mongodb.net/test?retryWrites=true&w=majority',
+  db:'mongodb://localhost/royal-shopping',
   port: process.env.PORT || 4000,
-  ip: process.env.IP || `https://cryptic-beach-01479.herokuapp.com`,
+  ip: process.env.IP || `https://127.0.0.1/4000`,
   imagepath: '/public/',
   jwtIssuer: 'www.yourdomainname.com',
-  jwtToken:'S1O2H2I4L5M6A7N8S9R1U2I3',
+  jwtToken:'hvjfhgfdgdfjkghncm,vngf5656454gfdg4d',
   jwtExpires: 86400 // Numeric seconds expiry (86400 = '1d'). If string supplied, you provide units (e.g., "8h")
+
 };
 // jwtExpires 86400=24hours=1day
 // https://www.google.com/search?q=24+hours+seconds&rlz=1C1CHBF_enIN860IN860&oq=24+hours+se&aqs=chrome.1.69i57j0l7.7441j0j7&sourceid=chrome&ie=UTF-8
