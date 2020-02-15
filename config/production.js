35dd8795398ae7176f55595db0cef4edbd317c3f
sohildev <58003771+sohildev@users.noreply.github.com>
2020-02-15 16:15:00 +0530

    Update production.js

--- config/production.js
+++ config/production.js
@@ -2,12 +2,12 @@
 
 module.exports = {
   env: 'production',
-  db: 'mongodb+srv://sohil:sohiL@5240@cluster0-kjwrv.mongodb.net/test?retryWrites=true&w=majority',
+  db:'mongodb://localhost/royal-shopping',
   port: process.env.PORT || 4000,
-  ip: process.env.IP || `https://cryptic-beach-01479.herokuapp.com`,
+  ip: process.env.IP || `https://127.0.0.1/4000`,
   imagepath: '/public/',
   jwtIssuer: 'www.yourdomainname.com',
-  jwtToken:'S1O2H2I4L5M6A7N8S9R1U2I3',
+  jwtToken:'hvjfhgfdgdfjkghncm,vngf5656454gfdg4d',
   jwtExpires: 86400 // Numeric seconds expiry (86400 = '1d'). If string supplied, you provide units (e.g., "8h")
 
 };
